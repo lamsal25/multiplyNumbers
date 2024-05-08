3d23c355000fbbd24be67f0b2597dcd91ad047a8
lamsal25 <144243529+lamsal25@users.noreply.github.com>
2024-05-08 16:15:32 +0545

    Update multiply.js

--- multiply.js
+++ multiply.js
@@ -4,6 +4,6 @@ function multiplyNumbers(a, b) {
 
 // Example usage:
 const num1 = 5;
-const num2 = 10;
+const num2 = 5;
 const result = multiplyNumbers(num1, num2);
 console.log("The result of multiplying", num1, "and", num2, "is:", result);

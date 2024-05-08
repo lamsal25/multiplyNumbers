8a69dfe22e5336dfddd165681e223a78f745bf38
lamsal25 <lamsalroshan7@gmail.com>
2024-05-08 16:08:39 +0545

    first commit

---
+++ multiply.js
@@ -0,0 +1,9 @@
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// Example usage:
+const num1 = 5;
+const num2 = 10;
+const result = multiplyNumbers(num1, num2);
+console.log("The result of multiplying", num1, "and", num2, "is:", result);
